c44aae533881dbb0f13ec5102eff08120d68b41d
Yuri <166571340+P3aar@users.noreply.github.com>
2025-02-26 19:34:03 -0300

    Opcao de sair
    Incluso no sistema a opcao de saida do usuario

--- Tarefa 2.cpp
+++ Tarefa 2.cpp
@@ -127,6 +127,8 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - sair do sistema\n\n");
+		
 		printf("Opcao: "); //fim do menu
 	
 		scanf("%d",&opcao); //armazenando a escolha do usuario
@@ -147,6 +149,12 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
+			
 			
 			default:
 			printf("essa opcao nao esta disponivel!\n");
